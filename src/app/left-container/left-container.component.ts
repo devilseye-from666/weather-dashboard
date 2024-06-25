ec350f59df1c4bcd412ef889a896bb3803542b0f
Dhritiman Nath <86851590+devilseye-from666@users.noreply.github.com>
2024-06-26 03:45:09 +0530

    added

--- src/app/left-container/left-container.component.ts
+++ src/app/left-container/left-container.component.ts
@@ -9,6 +9,7 @@ import { WeatherService } from '../Services/weather.service';
   selector: 'app-left-container',
   templateUrl: './left-container.component.html',
   styleUrls: ['./left-container.component.scss']
+  
 })
 export class LeftContainerComponent {
   faMagnifyingGlass: any = faMagnifyingGlass;
